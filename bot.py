7b069d7d0e0c521cdcee1af518682a29df820347
Satvik Sethia <satviksethia@gmail.com>
2019-02-19 19:35:08 -0600

    list of statuses not reversible

--- bot.py
+++ bot.py
@@ -35,7 +35,7 @@ def print_tweets():
 	# score = re.compile(r'[0-9]+\s*\-\s*[0-9]+')
 
 	while True:
-		for tweet in api.user_timeline(manutd.screen_name).reverse():
+		for tweet in api.user_timeline(manutd.screen_name):
 			try:
 				# Retweet tweets as they are found
 				tweet.retweet()
