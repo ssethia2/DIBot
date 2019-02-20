f496b19a2ea1c66a85e7a28b46bb984a937142b7
Satvik Sethia <satviksethia@gmail.com>
2019-02-19 19:05:49 -0600

    binding to port set by heroku

--- server.py
+++ server.py
@@ -2,5 +2,5 @@ from os import environ
 from flask import Flask
 
 app = Flask(__name__)
-app.run(environ.get('PORT'))
-app.run(host= '0.0.0.0', port=environ.get('PORT'))
+port = int(os.environ.get('PORT', 5000))
+app.run(host= '0.0.0.0', port=port)
